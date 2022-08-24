bc95469f94cf988b6726746a2388418a5729c9f5
dilhun.polat <dilhun.polat@GAMZEUYSAL>
2022-08-24 13:43:33 +0300

    Add project files.

---
+++ eTickets/Startup.cs
@@ -0,0 +1,50 @@
+﻿using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets
+{
+    public class Startup
+    {
+        public IConfiguration ConfigRoot
+        {
+            get;
+        }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public Startup(IConfiguration configuration)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+            ConfigRoot = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //DbContext Configuration
+
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
+
+            services.AddControllersWithViews();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            if (env is null)
+            {
+                throw new ArgumentNullException(nameof(env));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+            app.Run();
+        }
+    }
+}
